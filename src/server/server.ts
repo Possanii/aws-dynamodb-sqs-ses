89f032ee7736bf5b2ad5603931857a0e06a952f3
Possanii <gpossanii@outlook.com>
2024-12-10 17:22:11 -0300

    Refactor order placement to use factory function
    - Remove direct dependencies on DynamoOrdersRepository, SQSGateway, and SESGateway in server.ts
    - Introduce makePlaceOrder factory function to create PlaceOrder instance
    - Simplify server code by using the factory function
    
    This change improves modularity and makes it easier to modify dependencies
    or swap implementations in the future.

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,22 +1,11 @@
 import fastify from "fastify";
-import { PlaceOrder } from "../useCases/PlaceOrder";
 import { env } from "../config/env";
-import { DynamoOrdersRepository } from "../repository/DynamoOrdersRepository";
-import { SQSGateway } from "../gateways/SQSGateway";
-import { SESGateway } from "../gateways/SESGateway";
+import { makePlaceOrder } from "../factories/makePlaceOrder";
 
 const app = fastify();
 
 app.post("/orders", async (request, reply) => {
-  const dynamoOrdersRepository = new DynamoOrdersRepository();
-  const sqsGateway = new SQSGateway();
-  const sesGateway = new SESGateway();
-
-  const placeOrder = new PlaceOrder(
-    dynamoOrdersRepository,
-    sqsGateway,
-    sesGateway
-  );
+  const placeOrder = makePlaceOrder();
 
   const { orderId } = await placeOrder.execute();
 
